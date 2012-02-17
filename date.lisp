4ed28f24c1bc727347a709510eddc79714d3b066
Chris Howey <howeyc@gmail.com>
2012-02-16 20:39:00 -0600

    Store data in hash table

--- date.lisp
+++ date.lisp
@@ -8,3 +8,10 @@
     (decode-universal-time date)
     (declare (ignore secs hour minute))
     (format nil "~4D/~2,'0D/~2,'0D" year month day)))
+
+(defun iso-date (date)
+ (multiple-value-bind
+  (secs hour minute day month year)
+  (decode-universal-time date)
+  (declare (ignore secs hour minute))
+  (format nil "~4D-~2,'0D-~2,'0D" year month day)))
